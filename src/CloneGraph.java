520db374cf84683c24f0c158d86430b40f33cff0
AveryKL-1213 <averykl8235@gmail.com>
2022-03-12 12:03:44 +0800

    Update CloneGraph.java

--- src/CloneGraph.java
+++ src/CloneGraph.java
@@ -2,27 +2,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-class Node {
-    public int val;
-    public List<Node> neighbors;
-
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-
 public class CloneGraph {
+
+    public static class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
     public static void main(String[] args) {
         int[][] adjList = {{2, 4}, {1, 3}, {2, 4}, {1, 3}};
         ArrayList<Node> graph = new ArrayList<>();

82741eade2cf4853bf699cc1751dfc4f19e43eda
AveryKL-1213 <averykl8235@gmail.com>
2022-03-25 11:53:29 +0800

    Create TwoCityScheduling.java

---
+++ src/TwoCityScheduling.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class TwoCityScheduling {
+    public static void main(String[] args) {
+        int[][] costs = {{259, 770}, {448, 54}, {926, 667}, {184, 139}, {840, 118}, {577, 469}};
+        int ans = twoCityScheduledCost(costs);
+        System.out.println(ans);
+    }
+
+    public static int twoCityScheduledCost(int[][] costs) {
+        int ans = 0;
+        ArrayList<Integer> diffA = new ArrayList<>();
+        ArrayList<Integer> diffB = new ArrayList<>();
+        for (int[] cost : costs) {
+            if (cost[0] < cost[1]) {
+                ans += cost[0];
+                diffA.add(cost[1] - cost[0]);
+            } else {
+                ans += cost[1];
+                diffB.add(cost[0] - cost[1]);
+            }
+        }
+        ArrayList<Integer> diff;
+        if (diffA.size() > diffB.size()) {
+            diff = diffA;
+        } else {
+            diff = diffB;
+        }
+        diff.sort(Comparator.naturalOrder());
+        int sub = Math.abs(diffA.size() - diffB.size()) / 2;
+        int bias = 0;
+        while (diff.get(bias) < 0) {
+            bias++;
+        }
+        for (int i = 0; i < sub; i++) {
+            ans += diff.get(i + bias);
+        }
+        return ans;
+    }
+}

55cb51775b05eb572bec9b783c8ac690d8e33873
AveryKL-1213 <averykl8235@gmail.com>
2022-03-25 11:58:46 +0800

    Update TwoCityScheduling.java

--- src/TwoCityScheduling.java
+++ src/TwoCityScheduling.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class TwoCityScheduling {
@@ -10,28 +11,41 @@ public class TwoCityScheduling {
 
     public static int twoCityScheduledCost(int[][] costs) {
         int ans = 0;
-        ArrayList<Integer> diffA = new ArrayList<>();
-        ArrayList<Integer> diffB = new ArrayList<>();
+        int[] diff = new int[costs.length];
+        int i = 0;
         for (int[] cost : costs) {
-            if (cost[0] < cost[1]) {
-                ans += cost[0];
-                diffA.add(cost[1] - cost[0]);
-            } else {
-                ans += cost[1];
-                diffB.add(cost[0] - cost[1]);
-            }
+            ans += cost[0];
+            diff[i++] = cost[1] - cost[0];
         }
-        ArrayList<Integer> diff;
-        if (diffA.size() > diffB.size()) {
-            diff = diffA;
-        } else {
-            diff = diffB;
-        }
-        diff.sort(Comparator.naturalOrder());
-        int sub = Math.abs(diffA.size() - diffB.size()) / 2;
-        for (int i = 0; i < sub; i++) {
-            ans += diff.get(i);
+        Arrays.sort(diff);
+        for (int j = 0; j < i / 2; j++) {
+            ans += diff[j];
         }
         return ans;
+
+//        int ans = 0;
+//        ArrayList<Integer> diffA = new ArrayList<>();
+//        ArrayList<Integer> diffB = new ArrayList<>();
+//        for (int[] cost : costs) {
+//            if (cost[0] < cost[1]) {
+//                ans += cost[0];
+//                diffA.add(cost[1] - cost[0]);
+//            } else {
+//                ans += cost[1];
+//                diffB.add(cost[0] - cost[1]);
+//            }
+//        }
+//        ArrayList<Integer> diff;
+//        if (diffA.size() > diffB.size()) {
+//            diff = diffA;
+//        } else {
+//            diff = diffB;
+//        }
+//        diff.sort(Comparator.naturalOrder());
+//        int sub = Math.abs(diffA.size() - diffB.size()) / 2;
+//        for (int i = 0; i < sub; i++) {
+//            ans += diff.get(i);
+//        }
+//        return ans;
     }
 }
